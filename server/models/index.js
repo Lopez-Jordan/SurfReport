71cbb2fcc148d91d75bfc0c5d252dd25d969fae1
Jordan Lopez <jordanlopezemail@gmail.com>
2023-10-25 14:21:15 -0700

    location model is now finished

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+// import other models here
+
+// make associations here
+
+module.exports = {
+    // export here
+};
---
+++ server/models/location.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    lat: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+      unique: false,
+    },
+    long: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+      unique: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Location',
+  }
+);
+
+module.exports = Location;
